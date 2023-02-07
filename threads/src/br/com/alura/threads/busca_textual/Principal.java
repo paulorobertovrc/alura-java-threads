566afa57d33d741662dde933d526df15375183ba
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-02-07 09:09:09 -0400

    Aula 02 - Ordem de execução

--- threads/src/br/com/alura/threads/busca_textual/Principal.java
+++ threads/src/br/com/alura/threads/busca_textual/Principal.java
@@ -3,7 +3,7 @@ package br.com.alura.threads.busca_textual;
 public class Principal {
 	
 	public static void main(String[] args) {
-		String nome = "da";
+		String nome = "ana";
 		
 		Thread threadAssinaturas1 = new Thread(new TarefaBuscaTextual("assinaturas1.txt", nome));
 		Thread threadAssinaturas2 = new Thread(new TarefaBuscaTextual("assinaturas2.txt", nome));
